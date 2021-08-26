b0121376e066ccd2584812cfdf8edeb8ed1d0538
schwirkoski <schwirkoski@gmail.com>
2021-08-26 03:59:52 -0300

    criação do arq. dados.php

--- dados/dados.php
+++ dados/dados.php
@@ -13,25 +13,25 @@
 
 <?php
 $produtos = [
-    "bolo" =>[ 
-      "descricao"=>" ",
+    "massa" =>[ 
+      "descricao"=>"Os sorbets são sorvetes de massas feitos à base de água, sem nenhum tipo de gordura vegetal ou animal. Em sua fabricação, também não é usado leite nem derivados. Geralmente, são utilizadas polpas de frutas que trazem mais refrescância... ",
       'preco'=>16.99,
       
     ],
-    'cupcake'=>[
-      'descricao'=>" ",
-      'preco'=> 7.99,
+    'picole'=>[
+      'descricao'=>" Esse picolé é o mais comum entre todos e o mais simples. Basicamente, uma calda cremosa de frutas  congelada no palito   ",
+      'preco'=> 1.99,
       
     ], 
-    'artesanal'=>[
-      'descricao'=>" ",
-      'preco'=> 59.99,
+    'paleta'=>[
+      'descricao'=>"As paletas pesam cerca de 120g, maiores que os tradicionais picolés, e possuem diversos sabores e recheios. Não utilizam produtos químicos para garantir o sabor fiel dos ingredientes. No México estes picolés artesanais são bastante populares, vendidos em carrinhos de rua. Os sabores mais pedidos são os de fruta e o de chá de hibisco, já no Brasil um grande sucesso foram os recheios cremosos. E as nossas são mais cremosas que o normal... ",
+      'preco'=> 12.00,
       
     ],
   
     'brownie'=>[
-      'descricao'=>" ",
-      'preco'=> 9.99,
+      'descricao'=>" Brownie coberto de chocolate ao leite recheado de  sorvete  de creme ou de chocolate meio amargo cremoso, uma sensação gelada de sabores...   ",
+      'preco'=> 10.99,
       
     ],
     
