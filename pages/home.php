ed78fa073957c7b0cebf10efdcb881256541503d
schwirkoski <schwirkoski@gmail.com>
2021-08-26 04:18:04 -0300

    criação da pagina home.php

---
+++ pages/home.php
@@ -0,0 +1,39 @@
+<h2>Nossos Produtos</h2>
+
+<div class="produtos">
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="imagens/paleta.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Paleta </h5>
+    <p class="card-text"> Paleta Mexicana cremosa nos diversos sabores...  	</p><br><br>
+    <a href="./index.php?p=paleta" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/massa.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Sorbets</h5>
+    <p class="card-text">Nadinha de misturas prontas ou sabores artificiais, receitas exclusivas, feitas com ingredientes frescos...</p>
+    <a href="./index.php?p=massa"  class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/brownie.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Brownie Gelato</h5>
+    <p class="card-text">Sobremesa perfeita, Um Brownie coberto de chocolate  recheado de  sorvete  de creme...</p>
+    <a href="./index.php?p=brownies" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/picole.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Picolé</h5>
+    <p class="card-text">Tradicional Picolé Brasileiro , feito com frutas cremosas...</p><br><br>
+    <a href="./index.php?p=picole" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+</div>
