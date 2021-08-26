84811f36e8d45373195abb651332447077462b00
schwirkoski <schwirkoski@gmail.com>
2021-08-26 04:21:03 -0300

    criação da pagina massa.php

---
+++ pages/massa.php
@@ -0,0 +1,18 @@
+
+
+<h2>Sorbets</h2>
+
+<div class="main">
+    <aside class="texto">
+        <?php echo $produtos["massa"]["descricao"]?>
+    </aside>
+
+    <div>
+        <img class="img-prod"  src="./imagens/massa.jpg"  alt="imagem_de_bolos">
+        <div class="botao-valor">R$<?php echo $produtos["massa"]["preco"]?></div>
+        <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+
+
+
