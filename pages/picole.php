72ccd17a2ff848f2ca930cc973874addeaf004de
schwirkoski <schwirkoski@gmail.com>
2021-08-26 04:21:40 -0300

    criação da pagina picole.php

---
+++ pages/picole.php
@@ -0,0 +1,19 @@
+
+
+<h2>Picolé</h2>
+
+
+
+<div class="main">
+    <aside class="texto">
+        <?php echo $produtos["picole"]["descricao"]?>
+    </aside>
+
+    <div>
+    <img class="img-prod" src="./imagens/picole.jpg"  alt="imagem_de_picole">
+     <div class="botao-valor">R$<?php echo $produtos["picole"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+    </div>
+</div>
+</body>
+</html>
