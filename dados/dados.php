6fbb63e32bc387d9321d2f058599aa67815e49c5
schwirkoski <schwirkoski@gmail.com>
2021-08-26 00:46:20 -0300

    criação da pasta e arq.  dados.php.com

---
+++ dados/dados.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+     
+    </body>
+</html>
+
+<?php
+$produtos = [
+    "bolo" =>[ 
+      "descricao"=>" ",
+      'preco'=>16.99,
+      
+    ],
+    'cupcake'=>[
+      'descricao'=>" ",
+      'preco'=> 7.99,
+      
+    ], 
+    'artesanal'=>[
+      'descricao'=>" ",
+      'preco'=> 59.99,
+      
+    ],
+  
+    'brownie'=>[
+      'descricao'=>" ",
+      'preco'=> 9.99,
+      
+    ],
+    
+    
+  ];
+?>
