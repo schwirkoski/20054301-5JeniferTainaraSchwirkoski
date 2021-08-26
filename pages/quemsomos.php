5f7ad3e20a730d48df6e370e4293d58eae9030e6
schwirkoski <schwirkoski@gmail.com>
2021-08-26 05:12:18 -0300

    mudança no texto quem somos

--- pages/quemsomos.php
+++ pages/quemsomos.php
@@ -1,7 +1,11 @@
 
 <h2>Quem Somos</h2>
 <div class="main">
-
+A Gelatera Schwirkoski tem tornado mais saborosa a vida de pessoas que são fãs de um bom sorvete.
+Fundada em 2021 por Jenifer Schwirkoski, a loja mantém o seu padrão de qualidade utilizando-se de um processo de fabricação artesanal,
+com produtos naturais e sem o uso de gorduras hidrogenadas, conservantes e aromatizantes.
+Todos as nossas materias primas são naturais, utilizamos frutas frescas na fabricação das massas, paletas e picoles.
+Desde o começo essa era e sempre vai ser a essência da GELATERIA  SCHWIRKOSKI , o uso de protudos totalmente organicos.
 
 
 <p> 
