972b87a3bdad3c5571c9dd8043aa8f4eba11f38a
schwirkoski <schwirkoski@gmail.com>
2021-08-26 04:21:22 -0300

    criaçao da pag. paleta.php

---
+++ pages/paleta.php
@@ -0,0 +1,20 @@
+<h2>Paleta Mexicana</h2>
+
+
+
+
+
+<div class="main">
+ <aside class="texto">
+ <?php echo $produtos["paleta"]["descricao"]?>    
+ </aside>
+
+ <div>
+   <img class="img-prod" src="imagens/paleta.jpg" alt="imagens_de_paleta">
+   <div class="botao-valor">R$<?php echo $produtos["paleta"]["preco"]?></div>
+   <div class="botao-valor"><button class="comprar">Comprar</button></div>
+ </div>
+</div>
+
+</body>
+</html>
