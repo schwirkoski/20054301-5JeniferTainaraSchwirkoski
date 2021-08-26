e1b8b5aa1debfa2b35104cfa5eefb42c065b0643
schwirkoski <schwirkoski@gmail.com>
2021-08-26 04:20:32 -0300

    pag.  loc.php  com link googlemaps integrado

---
+++ pages/localizacao.php
@@ -0,0 +1,9 @@
+<h2>Nossa Localização</h2>
+<div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1092.7674889760156!2d-49.0830120677248!3d-26.488608167685914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94de94ec45c735ad%3A0xd528660baaf1e754!2sJaragu%C3%A1%20do%20Sul%20-%20Barra%20do%20Rio%20do%20Cerro%2C%20Jaragu%C3%A1%20do%20Sul%20-%20SC!5e0!3m2!1spt-BR!2sbr!4v1629960644355!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+
+
+  
+</body>
+</html>
